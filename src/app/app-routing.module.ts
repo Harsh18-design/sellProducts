05261701011051190f5cf01f00780777b3ad8ea0
harshsingh201 <harsh204815@gmail.com>
2024-01-26 13:46:48 +0530

    ecommerce laste commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CartComponent } from './cart/cart.component';
+import { OrderComponent } from './order/order.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductPageComponent // this is the component with the <router-outlet> in the template
+  },
+  {
+    path: 'test',
+    component: CheckoutComponent // this is the component with the <router-outlet> in the template
+  },
+  {
+    path: 'cart',
+    component: CartComponent // this is the component with the <router-outlet> in the template
+  },
+  {
+    path: 'order',
+    component: OrderComponent // this is the component with the <router-outlet> in the template
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
